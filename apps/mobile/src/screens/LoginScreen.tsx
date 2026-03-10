3fd63bd363562b2eebb9cbdc67d404d8783cc006
Aulakh1881 <anveeraulakh@gmail.com>
2026-03-09 23:02:40 -0400

    Create LoginScreen component with input fields and buttons
    This file implements the LoginScreen component with input fields for username and password, and buttons for login and sign up.

---
+++ apps/mobile/src/screens/LoginScreen.tsx
@@ -0,0 +1,49 @@
+// This is the login screen
+import React from 'react';
+import {StyleSheet, TextInput, View, Image, Text} from 'react-native';
+import ButtonComponent from '../components/button-style';
+import InputField from '../components/input-fields';
+import ImageLogo from '../components/login-logo';
+
+export default function LoginScreen() {
+    return (
+        <View style={styles.container}>
+            <ImageLogo />
+            <Text style={styles.labelDesign}>{'ProXIma'}</Text>
+            <Text style={styles.smallTextDesign}>{'Ensuring High-Speed Connectivity'}</Text>
+            <InputField placeHolderValue='Input Username'/>
+            <InputField placeHolderValue='Input Password'/>
+            <View style={styles.buttonLoginDesign}>
+                <ButtonComponent title="Login" actionWhenPressed={() => {}}/>
+                <ButtonComponent title="Sign Up" actionWhenPressed={() => {}}/>
+            </View>     
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  labelDesign:{
+    fontSize:50,
+    color: '#60a9da',
+    fontWeight: '500',
+    position: 'absolute',
+    top: 200,
+    fontStyle: 'italic',
+  },
+  smallTextDesign:{
+    color: '#60a9da',
+    position: 'absolute',
+    top: 270,
+  },
+  buttonLoginDesign: {
+    // Lines 39-41 were researched from Google Gemini
+    flexDirection: 'row',
+    gap: 20,
+    marginTop: 15,
+  },
+});
