b1895c9ad3dbc6ecb800a4eb381a5a57844620e8
Aulakh1881 <anveeraulakh@gmail.com>
2026-03-23 23:52:17 -0400

    Implement user registration functionality in SignUpScreen
    Added communication between sign up screen and supabase for user account creation.
    Also, for YOUR_LAPTOP_IP, use your actual computer ip address to test on your phone.

--- apps/mobile/src/screens/SignUpScreen.tsx
+++ apps/mobile/src/screens/SignUpScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, TextInput, View, Image, Text} from 'react-native';
+import {StyleSheet, TextInput, View, Image, Text, Alert} from 'react-native';
 import ButtonComponent from '../components/button-style';
 import InputField from '../components/input-fields';
 import { useNavigation } from '@react-navigation/native';
@@ -7,12 +7,45 @@ import { useNavigation } from '@react-navigation/native';
 //<Text style={styles.smallTextDesign}>{'Ensuring High-Speed Connectivity'}</Text>
 
 export default function SignUpScreen({ navigation }: any) {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [username, setUsername] = React.useState('');
+
+  const handleSignUp = async () => {
+    //Lines 15-25 were researched from Google Gemini
+    try{
+      // Send JSON format of user infromation to my laptop's server/supabase
+      const response = await fetch('http://YOUR_LAPTOP_IP:3000/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password, username }),
+      });
+
+      // Wait to get response from supabase/server
+      const data = await response.json();
+
+      // Lines 30-38 were researched from VScode
+      if (response.ok) {
+        Alert.alert("Success", data.message);
+        navigation.navigate('Login');
+      } else {
+        Alert.alert("Error", data.error || "Registration failed");
+      } 
+
+    } catch (error) {
+      Alert.alert('Error', 'Failed to create account. Please try again.');
+    }
+  }
+
   return(<View style={styles.container}>
         <Text style={styles.labelDesign}>{'Sign Up'}</Text>
-        <InputField placeHolderValue='Username'/>
-        <InputField placeHolderValue='Password'/>
+        <InputField placeHolderValue='Username' onChangeText={setUsername}/>
+        <InputField placeHolderValue='Email' onChangeText={setEmail}/>
+        <InputField placeHolderValue='Password' onChangeText={setPassword}/>
         <View style={styles.buttonLoginDesign}>
-            <ButtonComponent title="Create Account" actionWhenPressed={() => {}}/>
+            <ButtonComponent title="Create Account" actionWhenPressed={handleSignUp}/>
         </View> 
     </View>
   );
