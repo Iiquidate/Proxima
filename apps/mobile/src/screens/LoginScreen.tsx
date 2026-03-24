a5dab382cedf0743f99f6abbdf86e967795ffbc0
Aulakh1881 <anveeraulakh@gmail.com>
2026-03-23 23:53:57 -0400

    Implement login functionality with email and password
    Added communication between login screen and supabase for user authentication.
    Also, for YOUR_LAPTOP_IP, use your actual computer ip address to test on your phone.

--- apps/mobile/src/screens/LoginScreen.tsx
+++ apps/mobile/src/screens/LoginScreen.tsx
@@ -1,20 +1,52 @@
 // This is the login screen
 import React from 'react';
-import {StyleSheet, TextInput, View, Image, Text, Button} from 'react-native';
+import {StyleSheet, TextInput, View, Image, Text, Button, Alert} from 'react-native';
 import ButtonComponent from '../components/button-style';
 import InputField from '../components/input-fields';
 import ImageLogo from '../components/login-logo';
 import { useNavigation } from '@react-navigation/native';
 
 export default function LoginScreen({ navigation }: any) {
+
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    const handleLogin = async () => {
+      //Lines 15-25 were researched from Google Gemini
+      try{
+      // Send JSON format of user infromation to my laptop's server/supabase
+        const response = await fetch('http://YOUR_LAPTOP_IP:3000/auth/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+        });
+
+        // Wait to get response from supabase/server
+        const data = await response.json();
+
+        // Lines 30-38 were researched from VScode
+        if (response.ok) {
+          Alert.alert("Success", data.message);
+          navigation.navigate('MainApp');
+        } else {
+          Alert.alert("Error", data.error || "Login failed");
+        }
+      } catch (error) {
+        Alert.alert('Error', 'Server is unreachable. Check Terminal 2.');
+      }
+    };
+
+
     return (
         <View style={styles.container}>
             <ImageLogo />
             <Text style={styles.labelDesign}>{'ProXIma'}</Text>
-            <InputField placeHolderValue='Username'/>
-            <InputField placeHolderValue='Password'/>
+            <InputField placeHolderValue='Email' onChangeText={setEmail}/>
+            <InputField placeHolderValue='Password' onChangeText={setPassword}/>
             <View style={styles.buttonLoginDesign}>
-                <ButtonComponent title="Login" actionWhenPressed={() => navigation.navigate('MainApp')}/>
+                <ButtonComponent title="Login" actionWhenPressed={handleLogin}/>
                 <ButtonComponent title="Sign Up" actionWhenPressed={() => {navigation.navigate('SignUp')}}/>
             </View>  
         <View style={{ position: 'absolute', bottom: 40 }}>
