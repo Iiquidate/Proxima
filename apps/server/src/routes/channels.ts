71c623964df5c13c4b5099244c709fd8e2133543
Anveer Aulakh <anveeraulakh@gmail.com>
2026-04-12 19:03:34 -0400

    Add channels route

---
+++ apps/server/src/routes/channels.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import * as ChannelController from '../controllers/channelController';
+import { requireAuth } from '../middleware/requireAuth'; // need this to allow post and delete to utilize AuthUser instead of regular Request
+
+const router = Router();
+router.get('/official', ChannelController.getOfficialChannels);
+router.get('/nearby', ChannelController.getNearbyChannels);
+router.get('/:id', ChannelController.getChannelById);
+router.post('/', requireAuth, ChannelController.createChannel);
+router.delete('/:id', requireAuth, ChannelController.deleteChannel);
+
+export default router;
